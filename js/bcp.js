b2e6467fd01e8f41a90dcc33adec0cd714ddedc3
Michael Laszlo <git@michaellaszlo.com>
2015-05-29 23:55:41 -0400

    Accept hex input

--- js/bcp.js
+++ js/bcp.js
@@ -2,6 +2,21 @@ var BigColorPicker = {
   scale: 2
 };
 
+BigColorPicker.makeInputHandler = function (input) {
+  var pattern = /^[0-9a-f]{0,6}$/i;
+  return function () {
+    var value = input.previousValue;
+    if (value === undefined) {
+      value = input.previousValue = '';
+    }
+    if (!pattern.test(input.value)) {
+      input.value = input.previousValue;
+    } else {
+      input.previousValue = input.value;
+    }
+  };
+};
+
 BigColorPicker.load = function () {
   var g = BigColorPicker;
   M.make('div', { id: 'wrapper', into: document.body });
@@ -11,6 +26,9 @@ BigColorPicker.load = function () {
     blue: M.make('input', { id: 'blue', into: controls }),
     green: M.make('input', { id: 'green', into: controls })
   };
+  [g.input.red, g.input.blue, g.input.green].forEach(function (input) {
+    input.oninput = g.makeInputHandler(input);
+  });
 };
 
 window.onload = BigColorPicker.load;
