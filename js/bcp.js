403a36426ad4dc444f2de2653299bde906bd241d
Michael Laszlo <git@michaellaszlo.com>
2015-05-31 01:23:43 -0400

    Draw color rings

--- js/bcp.js
+++ js/bcp.js
@@ -3,13 +3,13 @@ var BigColorPicker = {
 };
 
 BigColorPicker.makeInputHandler = function (input) {
-  var pattern = /^[0-9a-f]{0,6}$/i;
+  var pattern = /^[0-9]{0,3}$/i;
   return function () {
     var value = input.previousValue;
     if (value === undefined) {
       value = input.previousValue = '';
     }
-    if (!pattern.test(input.value)) {
+    if (!pattern.test(input.value) || input.value > 255) {
       input.value = input.previousValue;
     } else {
       input.previousValue = input.value;
@@ -31,15 +31,40 @@ BigColorPicker.load = function () {
     blue: M.make('canvas', { id: 'blueRing', into: controls }),
     green: M.make('canvas', { id: 'greenRing', into: controls })
   };
-  ['red', 'blue', 'green'].forEach(function (color) {
+  ['red', 'blue', 'green'].forEach(function (color, ix, array) {
+    // Position the input box.
     var input = g.input[color];
     input.oninput = g.makeInputHandler(input);
+    if (ix != array.length-1) {
+      input.style.marginRight = 150 + 'px';
+    }
+    // Position the canvas.
     var canvas = g.ring[color];
-    canvas.width = canvas.height = 100;
-    canvas.style.left = canvas.offsetLeft + canvas.offsetWidth/2 -
-        input.offsetWidth/2 + 'px';
-    canvas.style.top = canvas.offsetTop + canvas.offsetTop/2 -
-        input.offsetTop/2 + 'px';
+    canvas.width = canvas.height = 240;
+    canvas.style.left = input.offsetLeft + input.offsetWidth/2 -
+        canvas.offsetWidth/2 + 'px';
+    canvas.style.top = input.offsetTop + input.offsetHeight/2 -
+        canvas.offsetHeight/2 + 'px';
+    // Draw the ring.
+    var context = canvas.getContext('2d'),
+        x = canvas.width/2,
+        y = canvas.height/2,
+        numSegments = 256,
+        increment = Math.PI*2/numSegments;
+    context.lineWidth = 30;
+    for (var i = 0; i < numSegments; ++i) {
+      context.beginPath();
+      var parts = ['#', '00', '00', '00'];
+      var hex = (numSegments-1-i).toString(16);
+      if (hex.length == 1) {
+        hex = '0'+hex;
+      }
+      parts[ix+1] = hex;
+      context.strokeStyle = parts.join('');
+      context.arc(x, y, 85,
+          -Math.PI/2 + i*increment, -Math.PI/2 + (i+1)*increment);
+      context.stroke();
+    }
   });
 };
 
