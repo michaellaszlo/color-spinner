8aae4fbfafc4691fe1285f67eac0af9bc3a3701a
Michael Laszlo <git@michaellaszlo.com>
2015-06-13 13:26:20 -0400

    Draw 256 notches around ring

--- js/spin.js
+++ js/spin.js
@@ -3,7 +3,8 @@ var ColorSpinner = {
   mix: {},
   layout: {
     canvas: { size: 240, gap: 10 },
-    ring: { width: 70, smoother: 2 },
+    ring: { width: 60, smoother: 2 },
+    notch: { width: 2 },
     hole: { overlap: 2 },
     display: { color: '#fff', width: 100, height: 32 },
     sector: { color: '#fff' }
@@ -99,21 +100,25 @@ ColorSpinner.load = function () {
   // Compute ring dimensions.
   var half = canvasSize/2,
       ringWidth = layout.ring.width,
+      notchWidth = layout.notch.width,
       smoother = layout.ring.smoother,
       overlap = layout.hole.overlap,
-      holeRadius = half - ringWidth - smoother;
+      holeRadius = half - ringWidth - notchWidth,
+      x0 = layout.canvas.size/2,
+      y0 = layout.canvas.size/2,
+      numSegments = 256,
+      increment = Math.PI*2/numSegments;
   layout.hole.radius = holeRadius;
   // Initialize the mixed color value.
   g.mix.rgb = [0, 0, 0];
   // Make a canvas for the mixed color.
   var mixCanvas = g.mix.canvas = M.make('canvas',
       { id: 'mix', into: wrapper });
-  mixCanvas.width = 4*holeRadius;
-  mixCanvas.height = 4*holeRadius;
-  mixCanvas.style.left = container.offsetLeft + 3*holeRadius + 'px';
-  mixCanvas.style.top = container.offsetTop + container.offsetHeight +
-      holeRadius + 'px';
-  // Make graphics for each color.
+  mixCanvas.width = container.offsetWidth;
+  mixCanvas.height = container.offsetWidth/2;
+  mixCanvas.style.left = container.offsetLeft + 'px';
+  mixCanvas.style.top = container.offsetTop + container.offsetHeight + 'px';
+  // Make a control disc for each color.
   g.colors.forEach(function (color, ix, array) {
     var cluster = g[color] = { index: ix };
     // Precompute color strings.
@@ -123,25 +128,29 @@ ColorSpinner.load = function () {
       rgb[ix] = i;
       colorStrings.push('rgb(' + rgb.join(', ') + ')');
     }
-    // Position the ring canvas.
+    // Compute canvas layout.
+    var canvasLeft = ix*(canvasSize + canvasGap) + 'px',
+        canvasTop = '0';
+    // Prepare the ring canvas.
     var ringCanvas = cluster.ring = M.make('canvas',
-        { id: color+'Ring', into: container }),
-        styleLeft = ix*(canvasSize + canvasGap) + 'px',
-        styleTop = '0';
+        { id: color+'Ring', into: container });
     ringCanvas.width = ringCanvas.height = layout.canvas.size;
-    ringCanvas.style.left = styleLeft;
-    ringCanvas.style.top = styleTop;
-    // Calculate the offset of the canvas with respect to the page.
-    var offset = cluster.offset = M.getOffset(ringCanvas, document.body);
-    // Draw the ring.
-    var context = ringCanvas.getContext('2d'),
-        x0 = ringCanvas.width/2;
-        y0 = ringCanvas.height/2;
-        numSegments = 256,
-        increment = Math.PI*2/numSegments;
+    ringCanvas.style.left = canvasLeft;
+    ringCanvas.style.top = canvasTop;
+    // Prepare the notch canvas.
+    var notchCanvas = cluster.notch = M.make('canvas',
+        { id: color+'Notch', into: container }),
+        notchContext = notchCanvas.getContext('2d');
+    notchCanvas.width = notchCanvas.height = layout.canvas.size;
+    notchCanvas.style.left = canvasLeft;
+    notchCanvas.style.top = canvasTop;
+    notchContext.lineWidth = notchWidth;
+    //notchContext.fillRect(0, 0, notchCanvas.width, notchCanvas.height);
     // Add the inside overlap and half the outside smoothing width.
     var renderWidth = overlap + ringWidth + smoother/2,
         renderCenter = holeRadius - overlap + renderWidth/2;
+    // Draw the ring and the notches.
+    var context = ringCanvas.getContext('2d');
     context.lineWidth = renderWidth;
     for (var i = 0; i < numSegments; ++i) {
       context.beginPath();
@@ -156,8 +165,13 @@ ColorSpinner.load = function () {
           endAngle = startAngle + (i == numSegments-1 ? 1 : 2) * increment;
       context.arc(x0, y0, renderCenter, startAngle, endAngle);
       context.stroke();
+      notchContext.strokeStyle = (i%2 == 0 ? '#777' : '#ddd');
+      notchContext.beginPath();
+      notchContext.arc(x0, y0, holeRadius+ringWidth, startAngle, endAngle);
+      notchContext.stroke();
     }
-    // Smooth the outer edge of the ring.
+    // Smooth the outer edge of the ring. The notches hide the smooth edge,
+    // so this only matters when the notch layer is invisible.
     context.strokeStyle = '#fff';
     context.lineWidth = smoother;
     context.beginPath();
@@ -167,24 +181,25 @@ ColorSpinner.load = function () {
     var holeCanvas = cluster.hole = M.make('canvas',
         { id: color+'Hole', into: container });
     holeCanvas.width = holeCanvas.height = layout.canvas.size;
-    holeCanvas.style.left = styleLeft;
-    holeCanvas.style.top = styleTop;
+    holeCanvas.style.left = canvasLeft;
+    holeCanvas.style.top = canvasTop;
     // Position the display box.
     var display = cluster.display = M.make('div',
-          { className: 'display', id: 'red', into: container,
-            innerHTML: '0' });
+          { className: 'display', into: container, innerHTML: '0' });
     display.style.width = displayWidth + 'px';
     display.style.height = displayHeight + 'px';
     display.style.left = ringCanvas.offsetLeft +
         (canvasSize - displayWidth)/2 + 'px';
     display.style.top = (canvasSize - displayHeight)/2 + 'px';
     display.style.color = layout.display.color;
+    // Calculate the offset of the canvas with respect to the page.
+    var offset = cluster.offset = M.getOffset(ringCanvas, document.body);
     // Position the touch canvas.
     var touchCanvas = cluster.touch = M.make('canvas',
         { id: color+'Touch', into: container });
     touchCanvas.width = touchCanvas.height = layout.canvas.size;
-    touchCanvas.style.left = styleLeft;
-    touchCanvas.style.top = styleTop;
+    touchCanvas.style.left = canvasLeft;
+    touchCanvas.style.top = canvasTop;
     touchCanvas.onmouseover = g.makeMouseHandler('over', color);
     touchCanvas.onmousemove = g.makeMouseHandler('move', color);
     touchCanvas.onmouseout = g.makeMouseHandler('out', color);
