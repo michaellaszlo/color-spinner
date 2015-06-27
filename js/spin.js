d45b06f452a8817b604ef62e8d62cd3edffc1522
Michael Laszlo <git@michaellaszlo.com>
2015-06-27 15:47:38 -0400

    Draw two-color mixing grid

--- js/spin.js
+++ js/spin.js
@@ -3,12 +3,13 @@ var ColorSpinner = {
   mix: { rgb: [0, 0, 0] },
   layout: {
     canvas: { width: 1100, height: 650, left: 0, top: 0, number: 5 },
-    mixer: { coarse: 1, diameter: 240, gap: 10 },
+    controls: { width: 300, height: 250 },
+    mixer: { coarse: 1, diameter: 200, gap: 10 },
     hole: { radius: { proportion: 0.4 } },
     smoother: 0.5,
     display: { color: '#fff', width: 100, height: 32 },
     sector: { color: '#fff', band: { proportion: 0.75 } },
-    grid: { coarse: 3, cell: 1, left: 0, top: 40 }
+    grid: { left: 10, coarse: 8, cell: 1, edge: 5 }
   },
 	show: { ring: { solid: false, mix: true } }
 };
@@ -20,88 +21,6 @@ ColorSpinner.toHex2 = function (i) {
   }
   return hex;
 }
-
-ColorSpinner.load = function () {
-  var g = ColorSpinner,
-      layout = g.layout;
-
-  // Allocate the drawing area.
-  var drawingArea = M.make('div', { id: 'drawingArea', into: document.body }),
-      canvases = g.canvases = [];
-  drawingArea.style.width = layout.canvas.width + 'px';
-  drawingArea.style.height = layout.canvas.height + 'px';
-
-  function makeMixer() {
-    var mixer = M.make('div', { className: 'mixer', into: drawingArea });
-    mixer.style.width = layout.mixer.diameter + 'px';
-    mixer.style.height = layout.mixer.diameter + 'px';
-    var canvases = mixer.canvases = [];
-    mixer.canvas = {};
-    mixer.context = {};
-    ['ring', 'hole', 'sector'].forEach(function (canvasName) {
-      var canvas = M.make('canvas', { into: mixer });
-      canvas.width = canvas.height = layout.mixer.diameter;
-      canvases.push(canvas);
-      mixer.canvas[canvasName] = canvas;
-      mixer.context[canvasName] = canvas.getContext('2d');
-    });
-    function mouseMove (event) {
-      var event = event || window.event;
-    }
-    mixer.onmouseover = function () {
-      mixer.onmousemove = mouseMove;
-    };
-    mixer.onmouseout = function () {
-      mixer.onmousemove = undefined;
-    };
-    mixer.onmousedown = function () {
-    };
-    return mixer;
-  }
-
-  // Make the three color discs.
-  g.mixer = {};
-  g.mixers = [];
-  g.colors.forEach(function (color, ix, array) {
-    var mixer = g.mixer[color] = makeMixer(),
-        diameter = layout.mixer.diameter,
-        width = diameter,
-        height = diameter;
-    g.mixers.push(mixer);
-    mixer.style.left = (ix+1)*layout.mixer.gap + ix*diameter + 'px';
-    mixer.style.top = layout.mixer.gap + 'px';
-    mixer.diameter = diameter;
-    mixer.color = color;
-    mixer.index = ix;
-    var label = mixer.label = M.make('div', { className: 'label',
-        into: mixer });
-    label.innerHTML = '256<br />xFF';
-    var labelWidth = label.offsetWidth,
-        labelHeight = label.offsetHeight;
-    label.style.width = labelWidth + 'px';
-    label.style.height = labelHeight + 'px';
-    label.style.left = (width - labelWidth)/2 + 'px';
-    label.style.top = (height - labelHeight)/2 + 'px';
-    mixer.setValue = g.makeMixerSetValue(mixer);
-    mixer.paint = g.makeMixerPaint(mixer);
-  });
-  g.colors.forEach(function (color, ix, array) {
-    g.mixer[color].setValue(Math.floor(Math.random() * 256));
-  });
-  g.colors.forEach(function (color, ix, array) {
-    g.mixer[color].paint();
-  });
-
-  // Make the two-color mixing grid.
-  var left = layout.grid.left,
-      top = layout.grid.top,
-      coarse = layout.grid.coarse,
-      cell = layout.grid.cell,
-      width = 256*cell,
-      height = 256*cell,
-      mixGrid = M.make('div', { into: drawingArea });
-
-};
 
 ColorSpinner.makeMixerSetValue = function (mixer) {
   var g = ColorSpinner;
@@ -172,6 +91,121 @@ ColorSpinner.makeMixerPaint = function (mixer) {
     //context.arc(x0, y0, holeRadius + bandWidth/2, angleFrom, angleTo);
     context.stroke();
   };
+};
+
+ColorSpinner.load = function () {
+  var g = ColorSpinner,
+      layout = g.layout;
+
+  // Allocate the drawing area.
+  var drawingArea = M.make('div', { id: 'drawingArea', into: document.body }),
+      canvases = g.canvases = [];
+  drawingArea.style.width = layout.canvas.width + 'px';
+  drawingArea.style.height = layout.canvas.height + 'px';
+
+  function makeMixer() {
+    var mixer = M.make('div', { className: 'mixer', into: drawingArea });
+    mixer.style.width = layout.mixer.diameter + 'px';
+    mixer.style.height = layout.mixer.diameter + 'px';
+    mixer.context = {};
+    ['ring', 'hole', 'sector'].forEach(function (canvasName) {
+      var canvas = M.make('canvas', { into: mixer });
+      canvas.width = canvas.height = layout.mixer.diameter;
+      mixer.context[canvasName] = canvas.getContext('2d');
+    });
+    function mouseMove (event) {
+      var event = event || window.event;
+    }
+    mixer.onmouseover = function () {
+      mixer.onmousemove = mouseMove;
+    };
+    mixer.onmouseout = function () {
+      mixer.onmousemove = undefined;
+    };
+    mixer.onmousedown = function () {
+    };
+    return mixer;
+  }
+
+  // Make the three color discs.
+  g.mixer = {};
+  g.mixers = [];
+  g.colors.forEach(function (color, ix, array) {
+    var mixer = g.mixer[color] = makeMixer(),
+        diameter = layout.mixer.diameter,
+        width = diameter,
+        height = diameter;
+    g.mixers.push(mixer);
+    mixer.style.left = layout.controls.width +
+        ix * (layout.mixer.gap + diameter) + 'px';
+    mixer.style.top = layout.mixer.gap + 'px';
+    mixer.diameter = diameter;
+    mixer.color = color;
+    mixer.index = ix;
+    var label = mixer.label = M.make('div', { className: 'label',
+        into: mixer });
+    label.innerHTML = '256<br />xFF';
+    var labelWidth = label.offsetWidth,
+        labelHeight = label.offsetHeight;
+    label.style.width = labelWidth + 'px';
+    label.style.height = labelHeight + 'px';
+    label.style.left = (width - labelWidth)/2 + 'px';
+    label.style.top = (height - labelHeight)/2 + 'px';
+    mixer.setValue = g.makeMixerSetValue(mixer);
+    mixer.paint = g.makeMixerPaint(mixer);
+  });
+
+  // Make the two-color mixing grid.
+  var coarse = layout.grid.coarse,
+      cell = layout.grid.cell,
+      gridSize = 256*cell,
+      edge = layout.grid.edge,
+      containerSize = 2*edge + gridSize,
+      mixGrid = M.make('div', { id: 'mixGrid', into: drawingArea });
+  mixGrid.style.left = layout.grid.left + 'px';
+  mixGrid.style.top = layout.controls.height + 'px';
+  mixGrid.style.width = containerSize + 'px';
+  mixGrid.style.height = containerSize + 'px';
+  mixGrid.context = {};
+  ['pixels'].forEach(function (canvasName) {
+    var canvas = M.make('canvas', { into: mixGrid });
+    canvas.width = canvas.height = containerSize;
+    mixGrid.context[canvasName] = canvas.getContext('2d');
+  });
+
+  context = mixGrid.context.pixels;
+  mixGrid.paint = function (holdIndex) {
+    var rgb = g.mix.rgb.slice(),
+        rowIndex = 1,
+        colIndex = 2;
+    if (holdIndex == 1) {
+      rowIndex = 0;
+      colIndex = 2;
+    } else if (holdIndex == 2) {
+      rowIndex = 0;
+      colIndex = 1;
+    }
+    for (r = 0; r < 256; r += coarse) {
+      rgb[rowIndex] = r;
+      for (c = 0; c < 256; c += coarse) {
+        rgb[colIndex] = c;
+        context.fillStyle = 'rgb(' + rgb.join(', ') + ')';
+        context.fillRect(c, r,
+            Math.min(256-c, coarse), Math.min(256-r, coarse));
+      }
+    }
+  }
+
+  // Choose a color at random.
+  g.colors.forEach(function (color, ix, array) {
+    g.mixer[color].setValue(Math.floor(Math.random() * 256));
+  });
+
+  // Paint the discs and the mixing grid.
+  g.colors.forEach(function (color, ix, array) {
+    g.mixer[color].paint();
+  });
+  mixGrid.paint();
 };
 
 window.onload = ColorSpinner.load;
