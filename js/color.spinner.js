c4a8573c9e74a118e5d8071ca1ad4362669f0200
Michael Laszlo <git@michaellaszlo.com>
2016-05-21 23:47:50 -0400

    Factor out hexagon painting

--- js/color.spinner.js
+++ js/color.spinner.js
@@ -143,16 +143,11 @@ HexagonPicker = (function () {
   var dimensions = {},
       canvases = {};
 
-  function paintHexagonFrame(canvas) {
+  function paintHexagon(canvas, x0, y0, radius, thickness, color) {
     var context = canvas.getContext('2d'),
-        width = canvas.width,
-        height = canvas.height,
-        radius = width / 2,
-        x0 = width / 2,
-        y0 = height / 2,
         i, angle, x, y;
     context.beginPath();
-    context.moveTo(width, y0);
+    context.moveTo(canvas.width, y0);
     for (i = 1; i <= 6; ++i) {
       angle = i * Math.PI / 3,
       x = x0 + Math.cos(angle) * radius,
@@ -162,6 +157,15 @@ HexagonPicker = (function () {
     context.lineWidth = 3;
     context.strokeStyle = '#888';
     context.stroke();
+  }
+
+  function paintHexagonFrame(canvas) {
+    var width = canvas.width,
+        height = canvas.height,
+        radius = width / 2,
+        x0 = width / 2,
+        y0 = height / 2;
+    paintHexagon(canvas, x0, y0, radius, 3, '#888');
   }
 
   function macroMouse(event) {
